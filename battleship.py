b9f9711ef1ef04e9dffee368e2973a39cca4f7c0
IvettSzoke <ivett.oze@gmail.com>
2022-01-24 14:34:10 +0100

    first settings

---
+++ battleship.py
@@ -0,0 +1,55 @@
+# placement phase
+
+def init_board():
+    board = [[ '0','0','0','0','0' ],[ '0','0','0','0','0' ],[ '0','0','0','0','0' ],[ '0','0','0','0','0' ],[ '0','0','0','0','0' ]]
+    # Ivett
+
+def ask_user_input():
+    # coordinates = input("\ncordinates ((A-E)(1-5)(horizontal/vertical)) or 'quit' for quit:\n")
+    # return coordinates
+    # 3 db input
+    # Ivett
+
+def is_input_valid():
+    # error msg when too close
+    # error msg when out of range (nem lehet 3 hosszu, G5 sem valid input)
+    # error wrong input (a-a)
+    # error msg for occupied coordinates
+    # call the input function
+    # Nóra
+    
+def place_ships():
+    # mark ship before the game
+    # Nóra
+
+def display_waiting_screen():
+    # screen is displayed with no boards, with the Next player's placement phase message
+    # Derien
+    
+def is_button_pressed():
+    # handle a character for starting - name a BUTTON
+    # True, False
+    # Derien
+
+def ready_board():
+    # check if all ships are placed
+    # Derien
+
+# shooting phase
+
+def ask_shooting_input():
+    # 2 for the coordinates
+    # Eszter
+
+def show_player_one_board():
+
+def show_player_two_board():
+
+def has_won():
+    won = True
+    # if S betu annyi mint a hajoink akkor nyert
+    # Eszter
+    
+def the_game():
+
+if __name__ == "__main__":
