745f253df435142fc835756e30fb5c85e5b744e0
IvettSzoke <ivett.oze@gmail.com>
2022-01-26 15:05:06 +0100

    fifth

--- battleship.py
+++ battleship.py
@@ -1,9 +1,9 @@
-# placement phase
 from colorama import Fore
 import re
+import tkinter as tk
 from tkinter import *
-import window
 
+#################### placement phase
 def ask_board_size():
     is_board_size_valid = False
     while not is_board_size_valid:
@@ -14,12 +14,6 @@ def ask_board_size():
         except:
             print(Fore.RED +"\nInvalid input! (must be between 5-10)")
     return board_size
-
-
-# def init_board(grid):
-#     board = [['0']*grid] * grid
-#     return board
-
 
 def init_board(grid):
     row = ['0']*grid
@@ -34,31 +28,29 @@ def print_board(board):
     for i in range(1,board_size+1):
         print (str(i), end =" ")
     print()
-    # print (' '.join([str(i) for i in range(1,board_size+1)]))
     rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     for index, row in enumerate(board):
         print(f'{rows[index]} {" ".join(row)}')
         
-
 def ask_user_input_for_ship_placement(grid):
     letters = "abcdefghijklmnopqrstuvvwxyz"
     max_letter = letters[grid-1]
 
     is_condition_letter_true = False
     while not is_condition_letter_true:
-        coordinate_letter = input(Fore.YELLOW + f"\nPlease specify the letter from the coordinate ((A-{max_letter.upper()})")
+        coordinate_letter = input(Fore.YELLOW + f"\nPlease specify the letter from the coordinate (A-{max_letter.upper()}) ")
         if re.match(f"[a-{letters[grid-1]}A-{letters[grid-1].upper()}]", coordinate_letter):
             is_condition_letter_true = True
     
     is_condition_number_true = False
     while not is_condition_number_true:
-        coordinate_number = input(Fore.WHITE + f"\nPlease specify the column of the starting coordinate (1-{grid})")
+        coordinate_number = input(Fore.WHITE + f"\nPlease specify the column of the starting coordinate (1-{grid}) ")
         try:
             coordinate_number = int(coordinate_number)
             if coordinate_number <= grid and coordinate_number > 0:
                 is_condition_number_true = True
         except:
-            print(Fore.RED + f"Please provide a number between 1 and {grid}!")
+            print(Fore.RED + f"Please provide a number between 1 and {grid}! ")
     
     is_condition_direction_true = False
     while not is_condition_direction_true:
@@ -68,51 +60,20 @@ def ask_user_input_for_ship_placement(grid):
             if direction == 'h' or direction == 'v':
                 is_condition_direction_true = True
         except:
-            print(Fore.RED + 'The direction has to be specified with the letters "h" or "v"!')
+            print(Fore.RED + 'The direction has to be specified with the letters "h" or "v"! ')
 
     return coordinate_letter, coordinate_number, direction
-
-# def is_input_valid():
-#     # error msg when too close
-#     # error msg when out of range (nem lehet 3 hosszu, G5 sem valid input)
-#     # error wrong input (a-a)
-#     # error msg for occupied coordinates
-#     # call the input function
-#     # Nora
     
 def available_ships_at_start(board_size):
     ship_config = {
-        5 : [1,2,3,3],
-        6 : [1,2,3,3,4],
-        7 : [1,2,2,3,3,4],
-        8 : [1,1,2,2,3,3,4],
-        9 : [1,1,2,2,2,3,3,4],
-        10: [1,1,1,1,2,2,2,3,3,4]
+        5 : [1,2],
+        6 : [1,2,3],
+        7 : [1,2,2,3],
+        8 : [1,1,2,2,3],
+        9 : [1,1,2,3,4],
+        10: [1,1,2,3,3,4]
     }
     return ship_config[board_size]
-
-# 10x10 (20): (1x4, 2x3, 3x2, 4x1)
-#   9x9 (18): (1x4, 2x3, 3x2, 2x1)
-#   8x8 (16): (1x4, 2x3, 2x2, 2x1)
-#   7x7 (15): (1x4, 2x3, 2x2, 1x1)
-#   6x6 (13): (1x4, 2x3, 1x2, 1x1)
-#   5x5 (11): (2x3, 1x2, 1x1)
-
-
-# def place_ships_player_0(board):
-#     player=0
-#     board_size = len(board[0])
-#     ships= available_ships_at_start(board_size)
-#     rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-#     print ('Available ships to place:', ships)
-#     for ship in ships:
-#         row_letter, col_number, direction = ask_user_input_for_ship_placement(board_size)
-#         #validation
-
-#         row_index = rows.index(row_letter.upper())
-#         col_index = int(col_number) - 1
-#         board[row_index][col_index] = "X"
-#         print_board(board)
 
 def place_ships(board):
     board_size = len(board[0])
@@ -120,14 +81,44 @@ def place_ships(board):
     rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     print ('Available ships to place:', ships)
     for ship in ships:
-        row_letter, col_number, direction = ask_user_input_for_ship_placement(board_size)
-        #validation
-        
-        row_index = rows.index(row_letter.upper())
-        col_index = int(col_number) - 1
+        while True:
+            row_letter, col_number, direction = ask_user_input_for_ship_placement(board_size)
+            row_index = rows.index(row_letter.upper())
+            col_index = int(col_number) - 1
+
+            if not is_in_range(board_size, ship, direction, row_index, col_index):
+                print(Fore.RED + "\nSorry, that's out of range.\n")
+                continue
+            elif not is_free(board, ship, direction, row_index, col_index):
+                print(Fore.RED + "\nIt's occupied!\n")
+                continue
+            else:
+                break
 
         mark_ship(board, ship, direction, row_index, col_index)
         print_board(board)
+
+def is_free(board, ship, direction, row_index, col_index):
+    for i in range(ship):
+        if board[row_index][col_index] == 'X':
+            return False
+        if direction == 'h':
+            col_index+=1
+        else:
+            row_index+=1
+    return True
+
+def is_in_range(board_size, ship, direction, row_index, col_index):
+    for i in range(ship):
+        if (row_index > board_size-1):
+            return False
+        elif(col_index > board_size-1):
+            return False
+        if direction == 'h':
+            col_index+=1
+        else:
+            row_index+=1
+    return True   
 
 def mark_ship(board, ship, direction, row_index, col_index):
     for i in range(ship):
@@ -143,7 +134,22 @@ def ready_board(occupied_coordinates, placement_area):
     else:
         False
 
-# shooting phase
+class Window():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Battleship")
+        self.root.geometry('400x100')
+        label = Label(self.root, text = "\nNext player's placement phase\n").pack()
+        button = tk.Button(self.root, 
+                          text = 'Next', 
+                          command=self.quit)
+        button.pack()
+        self.root.mainloop()
+
+    def quit(self):
+        self.root.destroy()
+
+########################### shooting phase
 
 def ask_shooting_input(grid):
     letters = "abcdefghijklmnopqrstuvvwxyz"
@@ -151,19 +157,19 @@ def ask_shooting_input(grid):
 
     is_shooting_letter_true = False
     while not is_shooting_letter_true:
-        shooting_letter = input(Fore.YELLOW + f"\nPlease specify the letter from the coordinate ((A-{max_letter.upper()})")
+        shooting_letter = input(Fore.YELLOW + f"\nPlease specify the letter from the coordinate (A-{max_letter.upper()}) ")
         if re.match(f"[a-{letters[grid-1]}A-{letters[grid-1].upper()}]", shooting_letter):
             is_shooting_letter_true = True
     
     is_shooting_number_true = False
     while not is_shooting_number_true:
-        coordinate_number = input(Fore.WHITE + f"\nPlease specify the column of the shooting coordinate (1-{grid})")
+        coordinate_number = input(Fore.WHITE + f"\nPlease specify the column of the shooting coordinate (1-{grid}) ")
         try:
             shooting_number = int(coordinate_number)
             if shooting_number <= grid and shooting_number > 0:
                 is_shooting_number_true = True
         except:
-            print(Fore.RED + f"Please provide a number between 1 and {grid}!")
+            print(Fore.RED + f"Please provide a number between 1 and {grid}! ")
     
     return shooting_letter, shooting_number
 
@@ -177,52 +183,56 @@ def show_player_2_board():
 def is_shooting_input_valid(guessed_pos):
     print(guessed_pos)
 
-# board = [[ '0','0','X','X','0' ],[ '0','0','0','0','0' ],[ '0','0','X','0','0' ],[ '0','0','0','0','0' ],[ '0','0','0','0','0' ]]
 
 def has_won(board, guessed_row, guessed_col,n):
     # won = False
-    # num_of_x_list = []
-
     # while not won:
+    
     rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     num_of_x = 0
+    num_of_s = 0
     for sub_list in board:        
-        num_of_x = num_of_x + sub_list.count('X')
+        num_of_x += sub_list.count('X')
+        num_of_s = num_of_s + sub_list.count('S')
     if num_of_x == 0:
         print(f"Player {n} wins!")
     row_i = rows.index(guessed_row)
     col_i = (int(guessed_col) - 1)
     if board[row_i][col_i] == '0':
         board[row_i][col_i] = 'M'
-        print("You've missed!") 
+        print(Fore.RED + "\nYou've missed!\n") 
     if board[row_i][col_i] in ['M', 'S', 'H']:
-        print('You guessed that one already.')
+        print(Fore.RED + "\nYou guessed that one already.\n")
     if board[row_i][col_i] == 'X':
-        print("You've hit a ship!")
+        print(Fore.GREEN + "\nYou've hit a ship!\n")
+    if board[row_i][col_i+1] == '0'and board[row_i+1][col_i] == '0' and board[row_i-1][col_i] == '0' and board[row_i][col_i-1] == '0':
+        board[row_i][col_i] == "S"
+        print("You've sunk a ship!")
+    # if board[row_i][col_i+1] == '0' and board[row_i-1][col_i] == '0' and board[row_i][col_i-1] == '0' and 
 
 
-    return won
+
+    # return won
    
-# ask_shooting_input()
-
 
 def the_game():
-    print(Fore.RED + "\nWelcome Players! This is the famous battleship! game!\n")
+    print(Fore.RED + "\nWelcome Players! This is the famous BATTLESHIP game!\n")
     grid = ask_board_size()
-    board = init_board(grid)
-    print_board(board)
-    place_ships(board)
+    board_1 = init_board(grid)
+    print_board(board_1)
+    place_ships(board_1)
 
-    exec(open('window.py').read())
+    # exec(open('window.py').read())
+    app = Window()
     
-    print(Fore.RED + "\nNext player's placement phase\n")
-    place_ships(board)
-    # ship_board_player_2 = place_ships(grid)
-
-
+    print(Fore.RED + "\nNext player's placement phase:\n")
+    board_2 = init_board(grid)
+    place_ships(board_2)
+    
     # initialize the covered boards
-    # covered_board_player_1 = 0
-    # covered_board_player_2 = 0
+    # covered_board_player_1 = board_1
+    # covered_board_player_2 = board_2
+    
 
     # # define number of turns. grid * grid ensures that some of the players will win
     # turns = grid * grid
@@ -261,10 +271,3 @@ def the_game():
 
 if __name__ == "__main__":
     the_game()
-
-
-# ask players for placing ships
-    # place ships function should be able to modify a board only independently from its owner. No need for player specific place ship function.
-    # the function should be able to handle the available ships argument and ask for user input until there is any available ship
-    # and it should ask for an additional user input (on top of coordinates and direction), which is the size
-    # finally it should place it in line with the game description (should not cover each other, should not be next to each other, ...)
